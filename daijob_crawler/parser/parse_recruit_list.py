6401ea3c5f63174a23e1cb39fc6bafebc663f619
Phạm Hải <sonhai0803@gmail.com>
2025-05-25 23:23:25 +0700

    crawl total page

--- daijob_crawler/parser/parse_recruit_list.py
+++ daijob_crawler/parser/parse_recruit_list.py
@@ -1,5 +1,5 @@
 from collections.abc import Iterable
-
+import math
 from salesnext_crawler.events import CrawlEvent, DataEvent, Event
 from scrapy.http.response.html import HtmlResponse
 from scrapy import Request
@@ -14,6 +14,9 @@ def parse_recruit_list(
     crawled_company_ids = event.metadata["crawled_company_ids"]
     crawled_recruit_ids = event.metadata["crawled_recruit_ids"]
     job_url = response.xpath("//div[@class='jobs_box mb16']//a/@href").getall()
+    total_page = int(response.xpath("//span[@class='roboto']/text()").get())
+    page = total_page / 20
+    page = math.ceil(page)
     urls = []
     for url in job_url:
         if 'jobs/detail/' in url:
@@ -21,14 +24,17 @@ def parse_recruit_list(
     urls = list(set(urls))
     next_page = response.xpath("//li[@class='next']/a/@href").get()
     next_page = response.urljoin(next_page)
-    if next_page:
-        yield CrawlEvent(
-            request=Request(next_page),
-            metadata={'crawled_recruit_ids': crawled_recruit_ids,
-                      'crawled_company_ids': crawled_company_ids},
-            callback=parse_recruit_list,
-        )
-    
+    if page:
+        for i in range(2, page):
+           yield CrawlEvent(
+                request=Request(f"https://www.daijob.com/jobs/search_result?job_post_language=2&job_search_form_hidden=1&page={i}"),
+                metadata={
+                    "crawled_company_ids": crawled_company_ids,
+                    "crawled_recruit_ids": crawled_recruit_ids,
+                },
+                callback=parse_recruit_list,
+            ) 
+            
     for url in urls:
         url = 'https://www.daijob.com' + url
         recruit_id = url.split('/')[-1]
